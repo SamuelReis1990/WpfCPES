0c67618a9aa367a37112b97aee851b7f7db1c5d5
Samuel_reis <samuelreis.1990@gmail.com>
2018-06-29 19:30:07 -0300

    Commit com melhorias no visual e implementação das imagens

--- WpfCPES/WpfCPES/MainCPES.xaml.cs
+++ WpfCPES/WpfCPES/MainCPES.xaml.cs
@@ -8,20 +8,19 @@ using System.Windows.Media;
 using CPES.Models;
 using System.Diagnostics;
 using System.Windows.Media.Animation;
-using System.Windows.Shapes;
 using System.Windows.Threading;
 
 namespace WpfCPES
 {
     /// <summary>
-    /// Interação lógica para MainWindow.xam
+    /// Interação lógica para MainCPES.xaml
     /// </summary>
     public partial class MainCPES : Window
     {
         Erro erro = new Erro();
-        StackPanel stackPanel;        
+        StackPanel stackPanel;
         DispatcherTimer dispatcherTimer;
-        Double value;
+        Double value;        
 
         public MainCPES()
         {
@@ -33,7 +32,7 @@ namespace WpfCPES
             btnCabecalhoMinimizar.BorderBrush = new SolidColorBrush(Color.FromRgb(42, 46, 50));
             txtArquivo1.Text = "...Informe a planilha das matrículas (PESQUISA)";
             txtArquivo2.Text = "...Informe a planilha dos endereços (RJCDC)";
-            txtArquivo3.Text = "...Informe a planilha das lotações (RJCD)";            
+            txtArquivo3.Text = "...Informe a planilha das lotações (RJCD)";
         }
 
         #region StackePanel Cabeçalho
@@ -81,7 +80,7 @@ namespace WpfCPES
             Storyboard.SetTarget(a, this.WindowsCPES);
             Storyboard.SetTargetProperty(a, new PropertyPath(OpacityProperty));
             storyboard.Completed += delegate { Close(); };
-            storyboard.Begin();            
+            storyboard.Begin();
         }
 
         #endregion
@@ -107,6 +106,8 @@ namespace WpfCPES
             txtArquivo1.Text = GetArquivos();
             if (!String.IsNullOrEmpty(txtArquivo1.Text))
             {
+                imgSetaEsquerda.Visibility = Visibility.Hidden;
+                imgSetaAcima.Visibility = Visibility.Visible;
                 btnArquivo2.IsEnabled = true;
                 stkArquivo2.Opacity = 1;
                 FecharMensagem();
@@ -140,6 +141,8 @@ namespace WpfCPES
             txtArquivo2.Text = GetArquivos();
             if (!String.IsNullOrEmpty(txtArquivo2.Text))
             {
+                imgSetaAcima.Visibility = Visibility.Hidden;
+                imgSetaDireita.Visibility = Visibility.Visible;
                 btnArquivo3.IsEnabled = true;
                 stkArquivo3.Opacity = 1;
                 FecharMensagem();
@@ -173,6 +176,8 @@ namespace WpfCPES
             txtArquivo3.Text = GetArquivos();
             if (!String.IsNullOrEmpty(txtArquivo3.Text))
             {
+                imgSetaDireita.Visibility = Visibility.Hidden;
+                imgSetaAbaixo.Visibility = Visibility.Visible;
                 btnExecutar.IsEnabled = true;
                 stkRodape.Opacity = 1;
                 FecharMensagem();
@@ -187,21 +192,7 @@ namespace WpfCPES
 
         #region StackePanel Mensagem
 
-        private void btnMensagem_MouseEnter(object sender, MouseEventArgs e)
-        {
-            var template = btnMensagem.Template;
-            var brdMensagem = (Border)template.FindName("brdMensagem", btnMensagem);
-            brdMensagem.Background = Brushes.LightBlue;
-        }
-
-        private void btnMensagem_MouseLeave(object sender, MouseEventArgs e)
-        {
-            var template = btnMensagem.Template;
-            var brdMensagem = (Border)template.FindName("brdMensagem", btnMensagem);
-            brdMensagem.Background = Brushes.LightGray;
-        }
-
-        private void btnMensagem_Click(object sender, RoutedEventArgs e)
+        private void txtMensagem_MouseLeave(object sender, MouseEventArgs e)
         {
             FecharMensagem();
         }
@@ -226,38 +217,52 @@ namespace WpfCPES
 
         private void btnExecutar_Click(object sender, RoutedEventArgs e)
         {
+            FecharMensagem();
+            var template = btnExecutar.Template;
+            var txtExecutar = (TextBlock)template.FindName("txtExecutar", btnExecutar);
+
+            imgSetaAbaixo.Visibility = Visibility.Hidden;
+            imgLoader.Visibility = Visibility.Visible;            
+            btnExecutar.IsEnabled = false;
+            stkRodape.Opacity = 0.5;
+            txtExecutar.Text = "Aguarde...";
+
+            #region Grava os parametros de entrada caso haja algum tipo de erro
+
+            erro.CaminhoArquivoParametro = txtArquivo1.Text;
+            erro.CaminhoArquivoEndereco = txtArquivo2.Text;
+            erro.CaminhoArquivoLotacao = txtArquivo3.Text;
+            erro.TabelaParametro = "PESQUISA";
+            erro.TabelaEndereco = "RJCDC";
+            erro.TabelaLotacao = "RJCD";
+            erro.NumColMatricula = 1;
+            erro.NumColMatriculaEndereco = 1;
+            erro.NumColMatriculaLotacao = 1;
+            erro.NumColNome = 2;
+            erro.NumColEndereco = 5;
+            erro.NumColNumero = 6;
+            erro.NumColComplemento = 7;
+            erro.NumColBairro = 8;
+            erro.NumColCidade = 9;
+            erro.NumColUF = 10;
+            erro.NumColCEP = 11;
+            erro.NumColLotacao = 26;
+
+            #endregion
+
+            dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Interval = TimeSpan.FromSeconds(10);
+            dispatcherTimer.Tick += new EventHandler(ExecucaoCPES);
+            dispatcherTimer.Start();
+        }
+
+        private void ExecucaoCPES(Object sender, EventArgs args)
+        {
             var template = btnExecutar.Template;
             var txtExecutar = (TextBlock)template.FindName("txtExecutar", btnExecutar);
 
             try
             {
-                btnExecutar.IsEnabled = false;
-                stkRodape.Opacity = 0.5;
-                txtExecutar.Text = "Aguarde...";
-
-                #region Grava os parametros de entrada caso haja algum tipo de erro
-
-                erro.CaminhoArquivoParametro = txtArquivo1.Text;
-                erro.CaminhoArquivoEndereco = txtArquivo2.Text;
-                erro.CaminhoArquivoLotacao = txtArquivo3.Text;
-                erro.TabelaParametro = "PESQUISA";
-                erro.TabelaEndereco = "RJCDC";
-                erro.TabelaLotacao = "RJCD";
-                erro.NumColMatricula = 1;
-                erro.NumColMatriculaEndereco = 1;
-                erro.NumColMatriculaLotacao = 1;
-                erro.NumColNome = 2;
-                erro.NumColEndereco = 5;
-                erro.NumColNumero = 6;
-                erro.NumColComplemento = 7;
-                erro.NumColBairro = 8;
-                erro.NumColCidade = 9;
-                erro.NumColUF = 10;
-                erro.NumColCEP = 11;
-                erro.NumColLotacao = 26;
-
-                #endregion   
-
                 var planilhaParametro = CPES.CPES.GetExcel(txtArquivo1.Text, "PESQUISA$", 1, 1, 1);
                 var planilhaEndereco = CPES.CPES.GetExcel(txtArquivo2.Text, "RJCDC$");
                 var planilhaLotacao = CPES.CPES.GetExcel(txtArquivo3.Text, "RJCD$", 1, 1, 1);
@@ -267,16 +272,18 @@ namespace WpfCPES
                 var colLotacao = planilhaLotacao.Select(s => s.Table.Columns).First();
 
                 string[] retorno = CPES.CPES.GetDados(planilhaParametro, planilhaEndereco, planilhaLotacao, colParametro, colEndereco, colLotacao);
+                dispatcherTimer.Stop();                
 
                 txtMensagem.Foreground = new SolidColorBrush(Color.FromRgb(60, 118, 61));
                 txtMensagem.Background = new SolidColorBrush(Color.FromRgb(223, 240, 216));
                 txtMensagem.Text = string.Empty;
                 txtMensagem.Text = string.Format("Operação realizada com sucesso!\nFoi gerado o arquivo {0} \nem {1} \ncom um total de {2} registros.", retorno[0], retorno[1], retorno[2]);
                 FadeIn(stkMensagem);
-
+                
+                imgLoader.Visibility = Visibility.Hidden;                
                 btnExecutar.IsEnabled = true;
                 stkRodape.Opacity = 1;
-                txtExecutar.Text = "Executar";
+                txtExecutar.Text = "Executar";                
             }
             catch (Exception ex)
             {
@@ -292,6 +299,7 @@ namespace WpfCPES
                 erro.Mensagem = ex.Message;
                 erro.StackTrace = textoStackTrace;
                 string[] retorno = CPES.CPES.CreateCsvErro(erro);
+                dispatcherTimer.Stop();
 
                 txtMensagem.Foreground = new SolidColorBrush(Color.FromRgb(169, 68, 66));
                 txtMensagem.Background = new SolidColorBrush(Color.FromRgb(242, 222, 222));
@@ -299,9 +307,10 @@ namespace WpfCPES
                 txtMensagem.Text = string.Format("Houve um erro!\nFoi gerado um arquivo de log de erro: {0} \nem {1} \nTente novamente mais tarde ou contacte o administrador do sistema.", retorno[0], retorno[1]);
                 FadeIn(stkMensagem);
 
+                imgLoader.Visibility = Visibility.Hidden;                
                 btnExecutar.IsEnabled = true;
                 stkRodape.Opacity = 1;
-                txtExecutar.Text = "Executar";
+                txtExecutar.Text = "Executar";                
             }
         }
 
@@ -363,7 +372,7 @@ namespace WpfCPES
             dispatcherTimer = new DispatcherTimer();
             dispatcherTimer.Interval = TimeSpan.FromMilliseconds(15);
             dispatcherTimer.Tick += new EventHandler(MoverObjeto);
-            dispatcherTimer.Start();            
+            dispatcherTimer.Start();
         }
 
         private void MoverObjeto(Object sender, EventArgs args)
@@ -375,7 +384,7 @@ namespace WpfCPES
             {
                 dispatcherTimer.Stop();
             }
-        }  
+        }
 
         #endregion
 
@@ -405,6 +414,6 @@ namespace WpfCPES
             FadeOut(stkMensagem);
         }
 
-        #endregion
+        #endregion       
     }
 }
